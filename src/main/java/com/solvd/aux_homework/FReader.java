17cd6639edfd802aa3e2551c8f3c83ecacdc2ddd
tgispert <spfury@gmail.com>
2023-12-03 10:17:06 -0300

    added functionality to FReader now it counts unique words and writes the total number of unique words in output.txt

--- src/main/java/com/solvd/aux_homework/FReader.java
+++ src/main/java/com/solvd/aux_homework/FReader.java
@@ -1,16 +1,22 @@
 package com.solvd.aux_homework;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class FReader {
     public static void main(String[] args) {
         try {
-            List<String> lines = FileUtils.readLines(new File("src/main/resources/1.txt"));
-            System.out.println(lines);
+            String text = FileUtils.readFileToString(new File("src/main/resources/input.txt"),"UTF-8");
+            Set<String> uniqueWords = new HashSet<>();
+            uniqueWords.addAll(Arrays.asList(StringUtils.split(text)));
+            int uniqueWordCount = uniqueWords.size();
+            FileUtils.writeStringToFile(new File("src/main/resources/output.txt"), "Number of unique words: " + uniqueWordCount, "UTF-8");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
